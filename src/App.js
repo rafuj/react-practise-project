5f29a3245812be8ed63d8c8e3e8386e666b23794
Abu Raihan Rafuj <raihanrafuj9978@gmail.com>
2022-01-07 05:14:29 +0600

    Complete Search Random User Project By React JS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.sass";
+
+const url = "https://randomuser.me/api";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const fetchRandomUser = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const responseData = await response.json();
+      setData(responseData);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomUser();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Searcing For Random User</h1>
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        {data.length !== 0 && (
+          <div className="author">
+            <div className="authorAvatar">
+              <img
+                src={data.results[0].picture.medium}
+                alt={data.results[0].name.first}
+              />
+            </div>
+            <div className="authorContent">
+              <h4 className="authorContentTitle">
+                <span>{data.results[0].name.title}</span>
+                <span>{data.results[0].name.first}</span>
+                <span>{data.results[0].name.last}</span>
+              </h4>
+            </div>
+          </div>
+        )}
+        <div className="buttonContainer">
+          <button onClick={fetchRandomUser}>Click Here</button>
+        </div>
+      </div>
+    </main>
   );
 }
 
